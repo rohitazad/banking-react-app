a58ec5120e1a01804e622a66b8de9ffb1575c618
Rohit Azad <ram9953878727@gmail.com>
2020-02-14 10:06:40 +0530

    Updates

---
+++ precache-manifest.2a30afb5ac7ffd73b2648f38c055c49c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03726f3a3b97b8dc91fb0a22861f245c",
+    "url": "/banking-react-app/index.html"
+  },
+  {
+    "revision": "d4b90cbf61b1aca2dec6",
+    "url": "/banking-react-app/static/css/2.571c7fb1.chunk.css"
+  },
+  {
+    "revision": "f4123b594d2d95b42ee3",
+    "url": "/banking-react-app/static/css/main.3830adaa.chunk.css"
+  },
+  {
+    "revision": "d4b90cbf61b1aca2dec6",
+    "url": "/banking-react-app/static/js/2.26dfb267.chunk.js"
+  },
+  {
+    "revision": "f4123b594d2d95b42ee3",
+    "url": "/banking-react-app/static/js/main.e1a71fe5.chunk.js"
+  },
+  {
+    "revision": "77476971b084b33e50b5",
+    "url": "/banking-react-app/static/js/runtime-main.d8c5d3e2.js"
+  },
+  {
+    "revision": "2ed3a3a83b393c7abc13c48bb758c505",
+    "url": "/banking-react-app/static/media/bg.2ed3a3a8.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/banking-react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/banking-react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/banking-react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/banking-react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/banking-react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
